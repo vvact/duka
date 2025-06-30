2ea84285ec4a6cc9d84b456b0234a217e870603b
vvact <vbm7752@gmail.com>
2025-06-30 13:10:59 +0300

    new commit

--- soko/settings/dev.py
+++ soko/settings/dev.py
@@ -1,9 +1,16 @@
 from .base import *
+from decouple import config
+import os
+
 
 DEBUG = True
+
+USE_CLOUDINARY=False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
+print("Local media root:", MEDIA_ROOT)
--- soko/settings/production.py
+++ soko/settings/production.py
@@ -1,7 +1,17 @@
 from .base import *
+from decouple import config
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
 
 
 DATABASES = {
